afe7cabb3a7adeb4c6731bf45ea57ed031346b39
sparagongf <muh.jawad.azzaki@gmail.com>
2020-05-19 02:24:36 +0700

    Added AJAX

--- application/views/p7login.php
+++ application/views/p7login.php
@@ -8,6 +8,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <title>P7 - Login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
     <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 <body>
     <div class="ui top menu">
